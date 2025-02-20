b6d0b97cfa546d7f902bfcdc4e9bf4665be623c6
bpour2s <bahman.pour@gmx.de>
2025-02-20 14:19:16 +0100

    initial commit

---
+++ db/db.js
@@ -0,0 +1,13 @@
+import {mongoose} from 'mongoose';
+import chalk from 'chalk';
+
+export default async function db() {
+    try {
+        const mongo=await mongoose.connect(process.env.MONGO_URI, {dbName: 'mieterunity'});
+        console.log(chalk.cyan('DB connected to ${mongo.connectio.name}' ));
+    } catch (error) {
+        console.log('error.massage');
+        throw new Error ('DB connection faild. Shouting down...');
+        
+    }
+}
