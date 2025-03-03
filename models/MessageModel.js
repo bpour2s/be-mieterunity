8636d2859328985d1fc1436190f30516f6850838
bpour2s <bahman.pour@gmx.de>
2025-03-03 15:00:17 +0100

    added openstreetmap api

--- models/MessageModel.js
+++ models/MessageModel.js
@@ -2,6 +2,19 @@ import { Schema, model } from "mongoose";
 import { UserModel } from "./UserModel.js";
 import { ReactionModel } from "./ReactionModel.js";
 
+
+const reactionsObject = new Schema({
+
+  userRefId: {
+    type: Schema.Types.ObjectId,
+    ref: "UserModel",
+  },
+  reactionRefId: {
+    type: Schema.Types.ObjectId,
+    ref: "ReactionModel",
+  },
+});
+
 const MessageSchema = new Schema(
   {
     message: {
@@ -16,35 +29,23 @@ const MessageSchema = new Schema(
       type: Schema.Types.ObjectId,
       ref: "UserModel",
     },
-    reactions: [
-      {
-        userId: {
-          type: Schema.Types.ObjectId,
-          ref: "UserModel",
-        },
-        reactionId: {
-          type: Schema.Types.ObjectId,
-          ref: "ReactionModel",
-        },
+    reactions: [reactionsObject],
 
-        status: {
-          type: String,
-          enum: [
-            "unread",
-            "read",
-            "seen",
-            "not_seen",
-            "answered",
-            "not_answered",
-          ],
-          default: "unread",
-        },
-      },
-    ],
-  },
-  {
-    timestaps: true,
-  }
+    status: {
+      type: String,
+      enum: [
+        "unread",
+        "read",
+        "seen",
+        "not_seen",
+        "answered",
+        "not_answered",
+      ],
+      default: "unread",
+    },
+  }, {
+  timestaps: true,
+}
 );
 
 const MessageModel = model("messages", MessageSchema);
