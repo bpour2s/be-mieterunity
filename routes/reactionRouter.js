863efc431533f8e781c0b84b0fea726aa9bc42f1
Andre Witt <dev.code@h-town.net>
2025-03-04 21:15:25 +0100

    refactoring AsyncHandler reactionRouter and controllers

--- routes/reactionRouter.js
+++ routes/reactionRouter.js
@@ -8,9 +8,12 @@ import {
   deleteOne,
 } from "../controllers/crudFactory.js";
 
+import { getAllReactions } from "../controllers/reactionControllers.js";
+
 const reactionRouter = Router();
 
-reactionRouter.get("/", getAll(ReactionModel));
+// reactionRouter.get("/", getAll(ReactionModel));
+reactionRouter.get("/", getAllReactions);
 reactionRouter.get("/:id", getOneById(ReactionModel));
 reactionRouter.post("/", createOne(ReactionModel));
 reactionRouter.put("/:id", updateOne(ReactionModel));
--- utils/asyncHandler.js
+++ utils/asyncHandler.js
@@ -1,7 +1,12 @@
 export default function asyncHandler(controller) {
-    return (req, res, next) => {
-      controller(req, res, next).catch((error) => {
-        next(error);
-      });
-    };
-  }
+  return (req, res, next) => {
+    res.set("Pragma", "no-cache");
+    res.set("Cache-Control", "no-cache");
+    res.set("Expires", "0");
+    res.set("ETag", null);
+    res.setHeader("Cache-Control", "no-store");
+    controller(req, res, next).catch((error) => {
+      next(error);
+    });
+  };
+}
